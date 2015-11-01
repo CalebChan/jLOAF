f5839038a2cf7dee981d92e99749fc9fb0718527
calebchan <calebchan@cmail.carleton.ca>
2015-10-31 22:27:42 -0400

    - Bug fixes

--- jLOAF/src/org/jLOAF/retrieve/JaccardDistanceRetrival.java
+++ jLOAF/src/org/jLOAF/retrieve/JaccardDistanceRetrival.java
@@ -2,7 +2,6 @@ package org.jLOAF.retrieve;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.jLOAF.casebase.Case;
 import org.jLOAF.casebase.ComplexCase;
 import org.jLOAF.sim.cases.SimilarityComplexCaseMetricStrategy;
@@ -20,28 +19,68 @@ public class JaccardDistanceRetrival extends SimilarityComplexCaseMetricStrategy
 		
 		List<Case> problemRun = new ArrayList<Case>();
 		problemRun.add(c1.getCurrentCase());
-		problemRun.addAll(c1.getPastCases());
+		
+		for (Case cc1 : c1.getPastCases()){
+			boolean isIn = false;
+			for (Case p : problemRun){
+				if (p.similarity(cc1) > threshold){
+					isIn = true;
+					break;
+				}
+			}
+			if (!isIn){
+				problemRun.add(cc1);
+			}
+		}
+		
+		//problemRun.addAll(c1.getPastCases());
 		
 		List<Case> candidateRun = new ArrayList<Case>();
 		candidateRun.add(c2.getCurrentCase());
-		candidateRun.addAll(c2.getPastCases());
 		
-		List<Case> smallRun = (problemRun.size() > candidateRun.size()) ? candidateRun : problemRun;
-		List<Case> largeRun = (problemRun.size() <= candidateRun.size()) ? candidateRun : problemRun;
+		for (Case cc1 : c2.getPastCases()){
+			boolean isIn = false;
+			for (Case c : candidateRun){
+				if (c.similarity(cc1) > threshold){
+					isIn = true;
+					break;
+				}
+			}
+			if (!isIn){
+				candidateRun.add(cc1);
+			}
+		}
+		//candidateRun.addAll(c2.getPastCases());
 		
-		ArrayList<Case> unionCase = new ArrayList<Case>();
-		for (Case pCase : smallRun){
-			for (Case cCase : largeRun){
-				if (pCase.similarity(cCase) > this.threshold){
-					if (!unionCase.contains(pCase)){
-						unionCase.add(pCase);
+		int min = Math.min(problemRun.size(), candidateRun.size());
+		ArrayList<Case> intersectCase = new ArrayList<Case>();
+		for (Case cCase : candidateRun){
+			for (Case pCase : problemRun){
+				double sim = pCase.similarity(cCase);
+				if (sim > this.threshold){
+					boolean isIn = false;
+					for (Case iCase : intersectCase){
+						double sim2 = iCase.similarity(pCase);
+						if (sim2 > threshold){
+							isIn = true;
+							break;
+						}
 					}
+					if (!isIn){
+						intersectCase.add(pCase);
+					}
+					if (intersectCase.size() > min){
+						System.out.println("MOO");
+					}
+					break;
 				}
 			}
 		}
-		double value = unionCase.size() * 1.0 / ((problemRun.size() + candidateRun.size() - unionCase.size()) * 1.0);
+			
+		double value = intersectCase.size() * 1.0 / ((problemRun.size() + candidateRun.size() - intersectCase.size()) * 1.0);
 		if (value > 1.0){
-			throw new RuntimeException("Value over 1.0 : " + value);
+//			throw new RuntimeException("Value over 1.0 : " + value);
+			System.out.println("ERROR");
 		}
 		return value;
 	}
